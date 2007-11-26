2bf6957a8ea93126ccf70073ec23733e855bd334
Gianni Valdambrini <aleister@develer.com>
2007-11-26 20:22:46 +0000

    separata la parte di gestione delle connessioni dal resto delle opzioni

--- src/devclient/gui_option.py
+++ src/devclient/gui_option.py
@@ -28,6 +28,169 @@ from PyQt4.QtGui import QApplication
 from storage import Storage
 from gui_option_ui import Ui_option
 
+class FormConnection(object):
+    """
+    Manage the connection part of gui option.
+    """
+
+    def __init__(self, widget, storage):
+        self.w = widget
+        self.storage = storage
+        self.w.port_conn.setValidator(QtGui.QIntValidator(self.w.port_conn))
+
+        self.connections = self.storage.connections()
+        for el in self.connections:
+            self.w.list_conn.addItem(el[1], QtCore.QVariant(el[0]))
+
+        self._translateText()
+        self._setupSignal()
+
+    def _translateText(self):
+        self._text = {}
+        self._text['name'] = QApplication.translate("option", "Name", None,
+                                                    QApplication.UnicodeUTF8)
+        self._text['host'] = QApplication.translate("option", "Host", None,
+                                                    QApplication.UnicodeUTF8)
+        self._text['port'] = QApplication.translate("option", "Port", None,
+                                                    QApplication.UnicodeUTF8)
+
+        self._text['connection'] = QApplication.translate("option",
+            "Connection", None, QApplication.UnicodeUTF8)
+
+        self._text['req_fields'] = QApplication.translate("option",
+            "The following fields are required", None, QApplication.UnicodeUTF8)
+
+        self._text['unique_name'] = QApplication.translate("option",
+            "Connection name must be unique", None, QApplication.UnicodeUTF8)
+
+    def _setupSignal(self):
+        clicked = SIGNAL("clicked()")
+        self.w.connect(self.w.save_conn, clicked, self.save)
+        self.w.connect(self.w.delete_conn, clicked, self.delete)
+        self.w.connect(self.w.list_conn, SIGNAL("currentIndexChanged(QString)"),
+                       self.load)
+
+        self.w.connect(self.w.connect_conn, clicked, self.connectReq)
+
+    def load(self, name):
+        """
+        Load data of one connection.
+
+        :Parameters:
+          name : str
+            the name of connection to load
+        """
+
+        conn = [el for el in self.connections if el[1] == name]
+
+        if conn:
+            n, h, p = conn[0][1:-1]
+            d = (QtCore.Qt.Unchecked, QtCore.Qt.Checked)[conn[0][-1]]
+            connect = True
+        else:
+            n, h, p, d = ('', '', '', QtCore.Qt.Unchecked)
+            connect = False
+
+        self.w.name_conn.setText(n)
+        self.w.host_conn.setText(h)
+        self.w.port_conn.setText(unicode(p))
+        self.w.connect_conn.setEnabled(connect)
+        self.w.default_conn.setCheckState(d)
+
+    def _checkFields(self):
+        """
+        Check validity of fields.
+        """
+
+        msg = []
+
+        conn_fields = {self._text['name']: self.w.name_conn,
+                       self._text['host']: self.w.host_conn,
+                       self._text['port']: self.w.port_conn}
+
+        for text, field in conn_fields.iteritems():
+            if not field.text():
+                msg.append(unicode(text))
+
+        if msg:
+            QtGui.QMessageBox.warning(self.w, self._text['connection'],
+                "%s:\n%s" % (self._text['req_fields'], '\n'.join(msg)))
+            return False
+        return True
+
+    def save(self):
+        """
+        Save a connection after check the fields validity.
+        """
+
+        if not self._checkFields():
+            return
+
+        if not self.w.list_conn.currentIndex():
+            id_conn = 0
+        else:
+            data = self.w.list_conn.itemData(self.w.list_conn.currentIndex())
+            id_conn = data.toInt()[0]
+
+        make_default = self.w.default_conn.checkState() == QtCore.Qt.Checked
+
+        if make_default:
+            for idx, conn in enumerate(self.connections):
+                if conn[0] != id_conn and conn[4]:
+                    c = list(conn)
+                    c[4] = 0
+                    self.storage.updateConnection(c)
+                    self.connections[idx] = tuple(c)
+
+        conn = [id_conn,
+                unicode(self.w.name_conn.text()),
+                unicode(self.w.host_conn.text()),
+                int(self.w.port_conn.text()),
+                int(make_default)]
+
+        if not self.w.list_conn.currentIndex():
+            if [el[1] for el in self.connections if
+                el[1] == self.w.name_conn.text()]:
+                QtGui.QMessageBox.warning(self.w, self._text['connection'],
+                                          self._text['unique_name'])
+                return
+            else:
+                self.storage.addConnection(conn)
+                self.w.list_conn.addItem(self.w.name_conn.text(),
+                                         QtCore.QVariant(conn[0]))
+                self.connections.append(conn)
+        else:
+            self.connections[self.w.list_conn.currentIndex() - 1] = conn
+            self.w.list_conn.setItemText(self.w.list_conn.currentIndex(),
+                                         conn[1])
+            self.storage.updateConnection(conn)
+
+        self.w.list_conn.setCurrentIndex(0)
+        self.load('')
+
+    def delete(self):
+        """
+        Erase a connection.
+        """
+
+        if not self.w.list_conn.currentIndex():
+            return
+
+        index = self.w.list_conn.currentIndex() - 1
+        self.storage.deleteConnection(self.connections[index])
+        self.w.list_conn.removeItem(self.w.list_conn.currentIndex())
+        del self.connections[index]
+
+    def connectReq(self):
+        """
+        Send a request to establish a connection.
+        """
+
+        id_conn = self.connections[self.w.list_conn.currentIndex() - 1][0]
+        self.w.emit(SIGNAL('connectReq(int)'), id_conn)
+        self.w.close()
+
+
 class GuiOption(QtGui.QDialog, Ui_option):
     """
     The Gui dialog for setup option.
@@ -36,25 +199,15 @@ class GuiOption(QtGui.QDialog, Ui_option):
     def __init__(self, parent):
         QtGui.QDialog.__init__(self, parent)
         self.setupUi(self)
-
-        self.port_conn.setValidator(QtGui.QIntValidator(self.port_conn))
-
         self._setupSignal()
         self.storage = Storage()
-        self._loadConnections()
+        self.conn = FormConnection(self, self.storage)
         self._translateText()
 
     def _setupSignal(self):
         clicked = SIGNAL("clicked()")
-        self.connect(self.save_conn, clicked, self._saveConnection)
-        self.connect(self.delete_conn, clicked, self._deleteConnection)
         self.connect(self.bg_button_style, clicked, self._chooseBgColor)
         self.connect(self.fg_button_style, clicked, self._chooseFgColor)
-
-        self.connect(self.list_conn, SIGNAL("currentIndexChanged(QString)"),
-                     self._loadConnection)
-
-        self.connect(self.connect_conn, clicked, self._connectReq)
         self.connect(self.tab_widget, SIGNAL("currentChanged(int)"),
                      self._syncTabs)
 
@@ -74,15 +227,7 @@ class GuiOption(QtGui.QDialog, Ui_option):
 
     def _translateText(self):
         self._text = {}
-        self._text['name'] = QApplication.translate("option", "Name", None,
-                                                    QApplication.UnicodeUTF8)
-        self._text['host'] = QApplication.translate("option", "Host", None,
-                                                    QApplication.UnicodeUTF8)
-        self._text['port'] = QApplication.translate("option", "Port", None,
-                                                    QApplication.UnicodeUTF8)
 
-        self._text['connection'] = QApplication.translate("option",
-            "Connection", None, QApplication.UnicodeUTF8)
         self._text['req_fields'] = QApplication.translate("option",
             "The following fields are required", None, QApplication.UnicodeUTF8)
         self._text['unique_name'] = QApplication.translate("option",
@@ -193,127 +338,3 @@ class GuiOption(QtGui.QDialog, Ui_option):
     def _chooseFgColor(self):
         color = QtGui.QColorDialog.getColor()
         self.fg_style.setText(color.name())
-
-    def _connectReq(self):
-        id_conn = self.connections[self.list_conn.currentIndex() - 1][0]
-        self.emit(SIGNAL('connectReq(int)'), id_conn)
-        self.close()
-
-    def _loadConnections(self):
-        """
-        Load all connections.
-        """
-
-        self.connections = self.storage.connections()
-        for el in self.connections:
-            self.list_conn.addItem(el[1], QtCore.QVariant(el[0]))
-
-    def _loadConnection(self, name):
-        """
-        Load data of one connection.
-
-        :Parameters:
-          name : str
-            the name of connection to load
-        """
-
-        conn = [el for el in self.connections if el[1] == name]
-
-        if conn:
-            n, h, p, d = conn[0][1:]
-            connect = True
-        else:
-            n, h, p, d = ('', '', '', QtCore.Qt.Unchecked)
-            connect = False
-
-        self.name_conn.setText(n)
-        self.host_conn.setText(h)
-        self.port_conn.setText(unicode(p))
-        self.connect_conn.setEnabled(connect)
-
-        if d:
-            self.default_conn.setCheckState(QtCore.Qt.Checked)
-        else:
-            self.default_conn.setCheckState(QtCore.Qt.Unchecked)
-
-    def _checkConnectionFields(self):
-        """
-        Check validity of connection fields.
-        """
-
-        msg = []
-
-        conn_fields = {self._text['name']: self.name_conn,
-                       self._text['host']: self.host_conn,
-                       self._text['port']: self.port_conn}
-
-        for text, field in conn_fields.iteritems():
-            if not field.text():
-                msg.append(unicode(text))
-
-        if msg:
-            QtGui.QMessageBox.warning(self, self._text['connection'],
-                "%s:\n%s" % (self._text['req_fields'], '\n'.join(msg)))
-            return False
-        return True
-
-    def _saveConnection(self):
-        """
-        Save a connection after check the fields validity.
-        """
-
-        if not self._checkConnectionFields():
-            return
-
-        if not self.list_conn.currentIndex():
-            id_conn = 0
-        else:
-            data = self.list_conn.itemData(self.list_conn.currentIndex())
-            id_conn = data.toInt()[0]
-
-        make_default = self.default_conn.checkState() == QtCore.Qt.Checked
-
-        if make_default:
-            for idx, conn in enumerate(self.connections):
-                if conn[0] != id_conn and conn[4]:
-                    c = list(conn)
-                    c[4] = 0
-                    self.storage.updateConnection(c)
-                    self.connections[idx] = tuple(c)
-
-        conn = [id_conn,
-                unicode(self.name_conn.text()),
-                unicode(self.host_conn.text()),
-                int(self.port_conn.text()),
-                int(make_default)]
-
-        if not self.list_conn.currentIndex():
-            if [el[1] for el in self.connections if
-                el[1] == self.name_conn.text()]:
-                QtGui.QMessageBox.warning(self, self._text['connection'],
-                                          self._text['unique_name'])
-            else:
-                self.storage.addConnection(conn)
-                self.list_conn.addItem(self.name_conn.text(),
-                                       QtCore.QVariant(conn[0]))
-                self.connections.append(conn)
-        else:
-            self.connections[self.list_conn.currentIndex() - 1] = conn
-            self.list_conn.setItemText(self.list_conn.currentIndex(), conn[1])
-            self.storage.updateConnection(conn)
-
-        self.list_conn.setCurrentIndex(0)
-        self._loadConnection('')
-
-    def _deleteConnection(self):
-        """
-        Erase a connection.
-        """
-
-        if not self.list_conn.currentIndex():
-            return
-
-        index = self.list_conn.currentIndex() - 1
-        self.storage.deleteConnection(self.connections[index])
-        self.list_conn.removeItem(self.list_conn.currentIndex())
-        del self.connections[index]
