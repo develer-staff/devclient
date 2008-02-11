dd74eaf3649caee6bdc3554f41d41ba6ac07a492
Gianni Valdambrini <aleister@develer.com>
2008-02-11 22:05:12 +0000

    aggiunto carattere wild

--- src/servers/clessidra.py
+++ src/servers/clessidra.py
@@ -22,6 +22,6 @@ from generics import *
 
 class Clessidra(DikuServer):
     right_widget = 'gui_map_ui'
-    wild_chars = '_~\^\|\.:x@\*\n\s'
+    wild_chars = '-_~\^\|\.:x@\*\n\s'
     host = 'mud.clessidra.it'
     port = 4000
