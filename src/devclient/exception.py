40be122609e43acd5bf346ff2a62eef3ad43e37a
Gianni Valdambrini <aleister@develer.com>
2008-03-12 18:13:34 +0000

    rimossa classe per le eccezioni non piu' utilizzata

--- src/devclient/exception.py
+++ src/devclient/exception.py
@@ -29,18 +29,6 @@ class DevClient(Exception):
     pass
 
 
-class ClassNotFound(DevClient):
-    """
-    Class for handling error on finding class to load.
-    """
-
-    def __init__(self, msg):
-        self.message = msg
-
-    def __str__(self):
-        return self.message
-
-
 class BufferUnderSize(DevClient):
     """
     Handling error on losing data because buffer size is not sufficient.
