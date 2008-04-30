b0a3cfdc07ce3925721f55f65e50f7d8730aef58
Gianni Valdambrini <aleister@develer.com>
2008-04-30 17:11:40 +0000

    corretta gestione del carattere NULL (No operation) del protocollo Telnet

--- src/devclient/constants.py
+++ src/devclient/constants.py
@@ -21,7 +21,7 @@
 __version__ = "$Revision$"[11:-2]
 __docformat__ = 'restructuredtext'
 
-PUBLIC_VERSION = '0.5.12'
+PUBLIC_VERSION = '0.5.13'
 """Public version of client."""
 
 PROJECT_NAME = 'DevClient'
