8b77ab392e556a5230675cc7128c76ddbe8d5f31
Gianni Valdambrini <aleister@develer.com>
2008-05-01 07:45:58 +0000

    corretto errore bloccante nella gestione del carattere NULL del protocollo telnet

--- src/devclient/constants.py
+++ src/devclient/constants.py
@@ -21,7 +21,7 @@
 __version__ = "$Revision$"[11:-2]
 __docformat__ = 'restructuredtext'
 
-PUBLIC_VERSION = '0.5.13'
+PUBLIC_VERSION = '0.5.14'
 """Public version of client."""
 
 PROJECT_NAME = 'DevClient'
