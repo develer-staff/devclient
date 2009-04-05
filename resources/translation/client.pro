f33a619d2ced92956e169a170c0aa9aae50b1e16
Gianni Valdambrini <aleister@develer.com>
2009-04-05 17:15:04 +0000

    ripristinate traduzioni rimosse per errore col commit 571

--- resources/translation/client.pro
+++ resources/translation/client.pro
@@ -3,11 +3,10 @@ TRANSLATIONS = devclient_it.ts
 SOURCES = ../../src/devclient/gui.msg \
           ../../src/devclient/gui_option.msg \
 	  ../../src/devclient/gui_src/gui.py \
-          ../../src/devclient/gui_src/gui_option.py
+          ../../src/devclient/gui_src/gui_option.py \
+          ../../src/devclient/gui_src/gui_map.py
 
-FORMS = ../../src/devclient/gui_src/gui_prompt.ui \
-        ../../src/devclient/gui_src/gui_map.ui \
-        ../../src/devclient/gui_src/gui_map_ext.ui \
+FORMS = ../../src/devclient/gui_src/gui_prompt.ui 
 
 
 CODECFORSRC = UTF-8
--- resources/translation/devclient_it.ts
+++ resources/translation/devclient_it.ts
@@ -1,56 +1,111 @@
-<!DOCTYPE TS><TS>
-<defaultcodec>UTF-8</defaultcodec>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="it">
+<defaultcodec></defaultcodec>
+<context>
+    <name>RightWidget</name>
+    <message>
+        <location filename="../../src/devclient/gui_src/gui_prompt.ui" line="19"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/devclient/gui_src/gui_map.py" line="68"/>
+        <source>Health</source>
+        <translation>Vita</translation>
+    </message>
+    <message>
+        <location filename="../../src/devclient/gui_src/gui_prompt.ui" line="89"/>
+        <source>QProgressBar { border: 2px solid gray; border-radius: 5px; }
+QProgressBar::chunk {background-color:#FF3333;}</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/devclient/gui_src/gui_map.py" line="81"/>
+        <source>Mana</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/devclient/gui_src/gui_prompt.ui" line="134"/>
+        <source>QProgressBar { border: 2px solid gray; border-radius: 5px; }
+QProgressBar::chunk {background-color:#0066FF}</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/devclient/gui_src/gui_map.py" line="94"/>
+        <source>Movement</source>
+        <translation>Movimento</translation>
+    </message>
+    <message>
+        <location filename="../../src/devclient/gui_src/gui_prompt.ui" line="179"/>
+        <source>QProgressBar { border: 2px solid gray; border-radius: 5px; }
+QProgressBar::chunk {background-color:#33CC33;}</source>
+        <translation></translation>
+    </message>
+</context>
 <context>
     <name>dev_client</name>
     <message>
+        <location filename="../../src/devclient/gui_src/gui.py" line="71"/>
         <source>Connect</source>
         <translation>Connetti</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui.py" line="80"/>
         <source>Option</source>
         <translation>Opzioni</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui.msg" line="7"/>
         <source>There aren&apos;t connections defined</source>
         <translation>Non ci sono connessioni definite</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui.msg" line="9"/>
         <source>Unable to establish connection</source>
         <translation>Impossibile stabilire la connessione</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui.py" line="31"/>
         <source>DevClient</source>
         <translation></translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui.msg" line="11"/>
         <source>Really quit?</source>
         <translation>Vuoi davvero uscire?</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui.msg" line="13"/>
         <source>Really close connection?</source>
         <translation>Vuoi davvero chiudere la connessione?</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui.msg" line="15"/>
         <source>Fatal Error, please restart your client</source>
         <translation>Errore irreversibile, riavviare il client</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui.msg" line="17"/>
         <source>Connection lost</source>
         <translation>Connessione persa</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui.msg" line="19"/>
         <source>Client is not connected</source>
         <translation>Il client non è connesso</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui.msg" line="19"/>
         <source>Unable to download updates</source>
         <translation>Impossibile scaricare gli aggiornamenti</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui.py" line="48"/>
         <source>Connection</source>
         <translation>Connessione</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui.py" line="63"/>
         <source>Account</source>
         <translation></translation>
     </message>
@@ -58,217 +113,270 @@
 <context>
     <name>option</name>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="575"/>
         <source>Option</source>
         <translation>Opzioni</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="556"/>
         <source>Connections</source>
         <translation>Connessioni</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="82"/>
         <source>Name</source>
         <translation>Nome</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="96"/>
         <source>Port</source>
         <translation>Porta</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="525"/>
         <source>Save</source>
         <translation>Salva</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="78"/>
         <source>Create New</source>
         <translation>Crea nuova</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="453"/>
         <source>Delete</source>
         <translation>Elimina</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="442"/>
         <source>Connection</source>
         <translation>Connessione</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_option.msg" line="4"/>
         <source>The following fields are required</source>
         <translation>E&apos; necessario specificare i campi</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_option.msg" line="11"/>
         <source>Connection name must be unique</source>
         <translation>Il nome della connessione deve essere unico</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="235"/>
         <source>Alias</source>
         <translation>Alias</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="245"/>
         <source>Label</source>
         <translation>Etichetta</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="255"/>
         <source>Body</source>
         <translation>Corpo</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_option.msg" line="23"/>
         <source>Create New</source>
         <comment>alias</comment>
         <translation>Crea nuovo</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="89"/>
         <source>Host</source>
         <translation></translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="350"/>
         <source>Macro</source>
         <translation></translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="351"/>
         <source>Keys</source>
         <translation>Tasti</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="446"/>
         <source>Command</source>
         <translation>Comando</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="353"/>
         <source>Register</source>
         <translation>Registra</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_option.msg" line="14"/>
         <source>Create New</source>
         <comment>macro</comment>
         <translation>Crea nuova</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_option.msg" line="17"/>
         <source>Key sequence must be unique</source>
         <translation>La sequenza dei tasti deve essere univoca</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="561"/>
         <source>Preferences</source>
         <translation>Preferenze</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="501"/>
         <source>Keep text entered</source>
         <translation>Tieni testo inserito</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="464"/>
         <source>Text inserted</source>
         <translation>Testo inserito</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="506"/>
         <source>General</source>
         <translation>Generale</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="512"/>
         <source>Save log</source>
         <translation>Salva log</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="557"/>
         <source>Accounts</source>
         <translation>Account</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="559"/>
         <source>Macros</source>
         <translation>Macro</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="558"/>
         <source>Aliases</source>
         <translation>Alias</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="147"/>
         <source>Account</source>
         <translation></translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="138"/>
         <source>Save accounts</source>
         <translation>Salva account</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="172"/>
         <source>Prompt</source>
         <translation></translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="197"/>
         <source>Normal</source>
         <translation>Normale</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="204"/>
         <source>Fight</source>
         <translation>Combattimento</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="160"/>
         <source>Change Prompt</source>
         <translation>Cambia Prompt</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_option.msg" line="13"/>
         <source>Are you sure to delete the connection?</source>
         <translation>Vuoi davvero eliminare la connessione?</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_option.msg" line="19"/>
         <source>Key sequence must be different from all the shortcut keys</source>
         <translation>La sequenza dei tasti deve essere diversa dalle scorciatoie</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="596"/>
         <source>Close</source>
         <translation>Chiudi</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="560"/>
         <source>Triggers</source>
         <translation>Trigger</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="443"/>
         <source>Trigger</source>
         <translation></translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="444"/>
         <source>Pattern</source>
         <translation></translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="445"/>
         <source>Ignore case</source>
         <translation>Ignora case</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_option.msg" line="29"/>
         <source>Create New</source>
         <comment>trigger</comment>
         <translation>Crea nuovo</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="447"/>
         <source>Change color to</source>
         <translation>Cambia colori con</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="448"/>
         <source>Text</source>
         <translation>Testo</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="450"/>
         <source>Background</source>
         <translation>Sfondo</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_option.msg" line="32"/>
         <source>At least one between text and background color is required</source>
         <translation>E&apos; necessario specificare almeno uno fra il colore del testo e dello sfondo</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="472"/>
         <source>Echo Color</source>
         <translation>Colore echo</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="483"/>
         <source>Command separator</source>
         <translation>Separatore comandi</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_option.msg" line="22"/>
         <source>Bad format on prompt</source>
         <translation>Formato del prompt errato</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_option.msg" line="28"/>
         <source>The label must be unique</source>
         <translation>L&apos;etichetta deve essere unica</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_option.msg" line="32"/>
         <source>The pattern must be unique</source>
         <translation>Il pattern deve essere univoco</translation>
     </message>
     <message>
+        <location filename="../../src/devclient/gui_src/gui_option.py" line="190"/>
         <source>&lt;table&gt;
 &lt;tr&gt;&lt;td colspan=3&gt;&lt;b&gt;Prompt format:&lt;/b&gt;&lt;/td&gt;&lt;/tr&gt;
 &lt;tr&gt;&lt;td&gt;%h&lt;/td&gt;&lt;td&gt;-&gt;&lt;/td&gt;&lt;td&gt;Current hit points&lt;/td&gt;&lt;/tr&gt;
