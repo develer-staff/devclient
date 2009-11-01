344617fe4bd407d1c3f5c2b6da20cebe31a99e06
Gianni Valdambrini <aleister@develer.com>
2009-11-01 11:47:44 +0000

    change the public version

--- src/devclient/constants.py
+++ src/devclient/constants.py
@@ -21,7 +21,7 @@
 __version__ = "$Revision$"[11:-2]
 __docformat__ = 'restructuredtext'
 
-PUBLIC_VERSION = '0.8.06'
+PUBLIC_VERSION = '0.8.07'
 """Public version of client."""
 
 PROJECT_NAME = 'DevClient'
